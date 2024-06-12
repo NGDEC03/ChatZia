36c73587609c82dd3d3927705018909b683f0b08
NGDEC03 <ngdec03@gmail.com>
2024-06-12 20:13:38 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import App from './App.jsx'
-import { useState } from 'react'
-function Login() {
-  const [isLoggedIn,setLogin]=useState(false)
-  const [userName,setName]=useState("")
-  return (
-   <div>
-    {isLoggedIn?<App userName={userName}/>:
-    <div>
-      <form onSubmit={
-        (e)=>{
-          e.preventDefault();
-          setLogin(true)
+import React, { useState } from 'react';
+import App from './App.jsx';
 
-      }}>
-        <input onChange={(e)=>setName(e.target.value)} value={userName} placeholder='Enter Your Name' className='bg-zinc-200'></input>
-      </form>
-      </div>
-    
-    
-    }
-   </div>
-  )
+function Login() {
+  const [isLoggedIn, setLogin] = useState(false);
+  const [userName, setName] = useState("");
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      {isLoggedIn ? (
+        <App userName={userName} />
+      ) : (
+        <form className="border-2 border-black p-4"onSubmit={(e) => {
+          e.preventDefault();
+          setLogin(true);
+        }}>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={userName}
+            placeholder='Enter Your Name'
+            className='bg-zinc-800 rounded-lg p-2'
+          />
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default Login;
