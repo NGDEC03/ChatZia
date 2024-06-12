b80db85f996903675e373b13e5814041fabb1537
NGDEC03 <ngdec03@gmail.com>
2024-06-12 20:14:44 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
             onChange={(e) => setName(e.target.value)}
             value={userName}
             placeholder='Enter Your Name'
-            className='bg-zinc-800 rounded-lg p-2'
+            className='bg-zinc-800 rounded-lg p-2 text-white'
           />
         </form>
       )}
